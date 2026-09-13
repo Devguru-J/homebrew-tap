f76a1cdde96dd63685ac1c8053841ae9d46da905
Devguru-J <hjk94610@gmail.com>
2026-09-13 19:48:11 +0900

    brewery 1.0.1

--- Casks/brewery.rb
+++ Casks/brewery.rb
@@ -1,6 +1,6 @@
 cask "brewery" do
-  version "1.0.0"
-  sha256 "e4ceea634dc29570de683f17056911dcbb0e3109d5833376e7ae8dc282e70c16"
+  version "1.0.1"
+  sha256 "b15d1979f3666f626104e0fb9648b7d6c76b7f7cc5c6504903988d4558a5029a"
 
   url "https://github.com/Devguru-J/brewery/releases/download/v#{version}/brewery-#{version}.zip"
   name "brewery"
